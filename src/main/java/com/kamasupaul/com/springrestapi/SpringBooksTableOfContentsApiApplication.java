1b4bf15ea2e6dc3d9001becdd761da4870852df1
Kamasu Paul <kamasupaul@gmail.com>
2022-07-31 20:06:42 +0300

    -- init app using spring intilizer

---
+++ src/main/java/com/kamasupaul/com/springrestapi/SpringBooksTableOfContentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kamasupaul.com.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooksTableOfContentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooksTableOfContentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamasupaul/com/springrestapi/SpringBooksTableOfContentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamasupaul.com.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBooksTableOfContentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
